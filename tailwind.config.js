13e35bd903415692f1a06aea9544311a7161f680
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-18 13:24:13 -0300

    uploading `next app` with `starter files`

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "hsl(233, 47%, 7%)",
+        "desaturated-blue": "hsl(244, 38%, 16%)",
+        "soft-violet": "hsl(277, 64%, 61%)",
+        "very-transparent-white": "hsla(0, 0%, 100%, 0.75)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.6)",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        deca: ["var(--font-lexend-deca)"],
+      },
+      fontSize: {
+        body: "15px",
+      },
+    },
+  },
+  plugins: [],
+};
