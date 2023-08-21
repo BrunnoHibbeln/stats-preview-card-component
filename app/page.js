05c641e50591e3adc51ca933747b37a6a3ad306b
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-21 11:38:03 -0300

    modifying the `height` in `main`

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ImageCard from "./components/ImageCard";
 
 export default function Home() {
   return (
-    <main className=" h-auto xl:h-[45%] w-full xl:w-3/4 flex flex-col-reverse xl:flex-row mx-5 sm:mx-16 md:mx-28 duration-700">
+    <main className=" h-auto xl:h-[45%] 2xl:h-[55%] w-full xl:w-3/4 flex flex-col-reverse xl:flex-row mx-5 sm:mx-16 md:mx-28 duration-700">
       <ArticleCard />
       <ImageCard />
     </main>
